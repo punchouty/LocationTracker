62744c246bf24687e51ac0818bc683ca0419d7d9
nehakapoor1 <nehakapahi@gmail.com>
2016-01-18 16:05:23 +0530

    First Commit

---
+++ src/main/java/com/racloop/LocationTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.racloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationTrackerApplication.class, args);
+	}
+}
